ef14fa70a7e02ee86a0b1326447bdf702f5034ed
Nishant-Kr-Shaw <shawnishantkr1@gmail.com>
2026-02-27 23:20:57 +0530

    Initial backend commit

--- backend/server.js
+++ backend/server.js
@@ -80,7 +80,7 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log('\n' + '='.repeat(60));
     console.log('KRISHI-ROUTE API SERVER');
     console.log('='.repeat(60));
